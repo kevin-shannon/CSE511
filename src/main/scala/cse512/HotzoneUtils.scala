cfd41eeabcbeae60d4c7eea75ef9c8af5dbe0000
arjunmakam <tmakam@asu.edu>
2021-06-26 16:27:35 -0700

    initial commit - HotzoneAnalysis

--- src/main/scala/cse512/HotzoneUtils.scala
+++ src/main/scala/cse512/HotzoneUtils.scala
@@ -3,8 +3,9 @@ package cse512
 object HotzoneUtils {
 
   def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
-    // YOU NEED TO CHANGE THIS PART
-    return true // YOU NEED TO CHANGE THIS PART
+    val Array(x1,y1,x2,y2)= queryRectangle.split(",").map(_.toDouble)
+    val Array(x,y)= pointString.split(",").map(_.toDouble)
+    return x >=x1 && x<= x2 && y>= y1 && y <=y2
   }
 
   // YOU NEED TO CHANGE THIS PART
