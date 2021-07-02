5051926bcc906877d2d3610279c78a924bc370d0
kevin-shannon <kevers429@gmail.com>
2021-07-02 12:16:12 -0700

    sort by G score

--- src/main/scala/cse512/HotcellAnalysis.scala
+++ src/main/scala/cse512/HotcellAnalysis.scala
@@ -43,10 +43,45 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
   import scala.math.sqrt
+  import spark.sqlContext.implicits._
   
   val x_bar = pickupInfo.agg(sum("count")).first.getLong(0).toDouble / numCells
-  val ssd = sqrt(pickupInfo.agg(sum("square count")).first.getLong(0).toDouble / numCells - x_bar*x_bar)
+  val s = sqrt(pickupInfo.agg(sum("square count")).first.getLong(0).toDouble / numCells - x_bar*x_bar)
+  var hotcells = Seq((0,0,0,0.0))
+  var xij = Array.ofDim[Int]((maxX - minX + 1).toInt,(maxY - minY + 1).toInt,(maxZ - minZ + 1))
 
-  return pickupInfo.select("x", "y", "z")
+  for (row <- pickupInfo.rdd.collect) {
+    var rowsplit = row.mkString(",").split(",")
+    xij(rowsplit(0).toInt - minX.toInt)(rowsplit(1).toInt - minY.toInt)(rowsplit(2).toInt - minZ.toInt) = rowsplit(3).toInt
+  }
+
+  for (x <- minX.toInt to maxX.toInt) {
+    for (y <- minY.toInt to maxY.toInt) {
+      for (z <- minZ to maxZ) {
+        var raw_score = 0.0
+        var neighbors = 0.0
+        for (dx <- -1 to 1) {
+          for (dy <- -1 to 1) {
+            for (dz <- -1 to 1) {
+              if (x+dx >= minX && x+dx <= maxX && y+dy >= minY && y+dy <= maxY && z+dz >= minZ && z+dz <= maxZ) {
+                raw_score += xij(x+dx - minX.toInt)(y+dy - minY.toInt)(z+dz - minZ.toInt)
+                neighbors += 1
+              }
+            }
+          }
+        }
+        // don't bother if the raw score is 0
+        if (raw_score != 0.0) {
+          var numerator = raw_score - x_bar * neighbors
+          var denominator = s * sqrt((numCells * neighbors - neighbors * neighbors)/(numCells - 1))
+          var g = numerator / denominator
+          hotcells = hotcells :+ (x,y,z,g)
+        }
+      }
+    }
+  }
+  val df = hotcells.toDF("x", "y", "z", "g")
+
+  return df.orderBy(col("g").desc).select("x", "y", "z")
 }
 }
